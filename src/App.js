9de4310c15a52ddef6b62ea5e45d0cb9c5f55997
guro <hack2943@gmail.com>
2024-05-31 16:13:43 +0900

    🥳 컴퍼넌트완료

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomeView from "./views/HomeView";
+
+const App = () => {
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomeView />} />
+        </Routes>
+    </BrowserRouter>
+    );
+};
+
+export default App;
